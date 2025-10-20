ffddb0444cfefc6d8ccbff44ba6ebca704086ed1
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-10-20 23:21:05 +0700

    feat: embroidery

--- lib/models/process/embroidery.dart
+++ lib/models/process/embroidery.dart
@@ -1,17 +1,21 @@
+import 'package:textile_tracking/helpers/service/base_crud_service.dart';
+
 class Embroidery {
   final int? id;
-  final String? dyeing_no;
+  final String? emb_no;
   final String? wo_no;
   final String? start_time;
   final int? start_by_id;
   final int? end_by_id;
   final String? end_time;
-  final String? qty;
+  final String? weight;
   final String? width;
   final String? length;
   final String? notes;
   final String? status;
-  final int? unit_id;
+  final int? weight_unit_id;
+  final int? width_unit_id;
+  final int? length_unit_id;
   final int? wo_id;
   final int? machine_id;
   final attachments;
@@ -21,15 +25,17 @@ class Embroidery {
 
   Embroidery(
       {this.id,
-      this.dyeing_no,
+      this.emb_no,
       this.start_time,
       this.end_time,
-      this.qty,
+      this.weight,
       this.width,
       this.length,
       this.notes,
       this.status,
-      this.unit_id,
+      this.weight_unit_id,
+      this.length_unit_id,
+      this.width_unit_id,
       this.wo_id,
       this.machine_id,
       this.start_by_id,
@@ -43,15 +49,17 @@ class Embroidery {
   factory Embroidery.fromJson(Map<String, dynamic> json) {
     return Embroidery(
       id: json['id'] as int?,
-      unit_id: json['unit_id'] as int?,
+      weight_unit_id: json['weight_unit_id'] as int?,
+      width_unit_id: json['width_unit_id'] as int?,
+      length_unit_id: json['length_unit_id'] as int?,
       wo_id: json['wo_id'] as int?,
       machine_id: json['machine_id'] as int?,
       start_by_id: json['start_by_id'] as int?,
       end_by_id: json['end_by_id'] as int?,
-      dyeing_no: json['dyeing_no'] ?? '',
+      emb_no: json['emb_no'] ?? '',
       start_time: json['start_time'] ?? '',
       end_time: json['end_time'] ?? '',
-      qty: json['qty'] ?? '',
+      weight: json['weight'] ?? '',
       width: json['width'] ?? '',
       length: json['length'] ?? '',
       status: json['status'] ?? '',
@@ -62,4 +70,39 @@ class Embroidery {
       end_by: json['end_by'],
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'weight_unit_id': weight_unit_id,
+      'length_unit_id': length_unit_id,
+      'width_unit_id': width_unit_id,
+      'wo_id': wo_id,
+      'machine_id': machine_id,
+      'start_by_id': start_by_id,
+      'end_by_id': end_by_id,
+      'emb_no': emb_no,
+      'start_time': start_time,
+      'end_time': end_time,
+      'wo_no': wo_no,
+      'weight': weight,
+      'width': width,
+      'length': length,
+      'notes': notes,
+      'status': status,
+      'attachments': attachments,
+      'start_by': start_by,
+      'end_by': end_by,
+      'work_orders': work_orders,
+    };
+  }
+}
+
+class EmbroideryService extends BaseCrudService<Embroidery> {
+  EmbroideryService()
+      : super(
+          endpoint: 'embroideries',
+          fromJson: (json) => Embroidery.fromJson(json),
+          toJson: (item) => item.toJson(),
+        );
 }

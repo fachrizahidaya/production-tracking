946048765e5e428bcf95ac31da2a5c9f4b4b1226
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-01-05 06:00:15 +0700

    fix: rework

--- lib/components/dyeing/rework/list_form.dart
+++ lib/components/dyeing/rework/list_form.dart
@@ -1,63 +1,35 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
-import 'package:textile_tracking/components/dyeing/create/info_tab.dart';
-import 'package:textile_tracking/components/dyeing/create/item_tab.dart';
 import 'package:textile_tracking/components/master/form/select_form.dart';
 import 'package:textile_tracking/components/master/layout/card/custom_card.dart';
-import 'package:textile_tracking/helpers/util/padding_column.dart';
+import 'package:textile_tracking/components/master/theme.dart';
 import 'package:textile_tracking/helpers/util/separated_column.dart';
 
 class ListForm extends StatefulWidget {
   final formKey;
+  final id;
   final form;
   final data;
-  final id;
-  final dyeingId;
-  final length;
-  final width;
-  final qty;
-  final note;
-  final handleSelectWo;
-  final handleChangeInput;
-  final handleSelectUnit;
-  final handleSelectMachine;
+  final attachments;
+  final selectWorkOrder;
+  final selectMachine;
   final isSubmitting;
-  final handleSubmit;
   final isFormIncomplete;
-  final isChanged;
-  final initialQty;
-  final initialLength;
-  final initialWidth;
-  final initialNotes;
-  final allAttachments;
+  final handleSubmit;
   final handlePickAttachments;
 
   const ListForm(
       {super.key,
       this.formKey,
       this.id,
-      this.handleSelectWo,
       this.form,
       this.data,
-      this.length,
-      this.width,
-      this.qty,
-      this.note,
-      this.handleChangeInput,
-      this.handleSelectUnit,
+      this.selectWorkOrder,
+      this.selectMachine,
       this.isSubmitting,
-      this.handleSelectMachine,
-      this.handleSubmit,
       this.isFormIncomplete,
-      this.dyeingId,
-      this.isChanged,
-      this.initialQty,
-      this.initialLength,
-      this.initialWidth,
-      this.initialNotes,
-      this.allAttachments,
-      this.handlePickAttachments});
+      this.handleSubmit,
+      this.handlePickAttachments,
+      this.attachments});
 
   @override
   State<ListForm> createState() => _ListFormState();
@@ -65,75 +37,31 @@ class ListForm extends StatefulWidget {
 
 class _ListFormState extends State<ListForm> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(8),
-            child: Form(
-              key: widget.formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  if (widget.id == null)
-                    CustomCard(
-                        child: Padding(
-                      padding: PaddingColumn.screen,
-                      child: SelectForm(
-                          label: 'Work Order',
-                          onTap: () => widget.handleSelectWo(),
-                          selectedLabel: widget.form['no_wo'] ?? '',
-                          selectedValue: widget.form['wo_id']?.toString() ?? '',
-                          required: false),
-                    )),
-                  if (widget.form?['wo_id'] != null)
-                    CustomCard(
-                        child: Padding(
-                      padding: PaddingColumn.screen,
-                      child: DefaultTabController(
-                          length: 2,
-                          child: Column(
-                            children: [
-                              TabBar(tabs: [
-                                Tab(
-                                  text: 'Informasi',
-                                ),
-                                Tab(
-                                  text: 'Barang',
-                                ),
-                              ]),
-                              SizedBox(
-                                  height: 400,
-                                  child: TabBarView(children: [
-                                    InfoTab(
-                                      data: widget.data,
-                                    ),
-                                    ItemTab(
-                                      data: widget.data,
-                                    )
-                                  ]))
-                            ],
-                          )),
-                    )),
-                  CustomCard(
-                      child: Padding(
-                    padding: PaddingColumn.screen,
-                    child: SelectForm(
-                      label: 'Mesin',
-                      onTap: () => widget.handleSelectMachine(),
-                      selectedLabel: widget.form['nama_mesin'] ?? '',
-                      selectedValue: widget.form['machine_id'].toString(),
-                      required: false,
-                    ),
-                  )),
-                ].separatedBy(SizedBox(
-                  height: 16,
-                )),
-              ),
-            )));
+    return Form(
+      key: widget.formKey,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            if (widget.id == null)
+              CustomCard(
+                  child: SelectForm(
+                label: 'Work Order',
+                onTap: () => widget.selectWorkOrder(),
+                selectedLabel: widget.form?['no_wo'] ?? '',
+                selectedValue: widget.form?['wo_id']?.toString() ?? '',
+                required: true,
+              )),
+            CustomCard(
+                child: SelectForm(
+              label: 'Mesin',
+              onTap: () => widget.selectMachine(),
+              selectedLabel: widget.form['nama_mesin'] ?? '',
+              selectedValue: widget.form['machine_id'].toString(),
+              required: true,
+            )),
+          ].separatedBy(CustomTheme().vGap('xl'))),
+    );
   }
 }

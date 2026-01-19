caa09dd26b760cfa9320f85491dfbf2305ef0d0c
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-01-20 05:45:20 +0700

    fix: dyeing

--- lib/components/dyeing/rework/rework_info_tab.dart
+++ lib/components/dyeing/rework/rework_info_tab.dart
@@ -1,13 +1,8 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:textile_tracking/components/dyeing/rework/create_form.dart';
 import 'package:textile_tracking/components/dyeing/rework/wo_item_tab.dart';
-import 'package:textile_tracking/components/master/card/custom_card.dart';
-import 'package:textile_tracking/components/master/card/custom_badge.dart';
-import 'package:textile_tracking/components/master/text/view_text.dart';
 import 'package:textile_tracking/components/master/theme.dart';
 import 'package:textile_tracking/components/process/info_tab.dart';
-import 'package:textile_tracking/helpers/util/format_html.dart';
 import 'package:textile_tracking/helpers/util/separated_column.dart';
 
 class ReworkInfoTab extends StatefulWidget {
--- lib/screens/dyeing/index.dart
+++ lib/screens/dyeing/index.dart
@@ -210,12 +210,10 @@ class _DyeingScreenState extends State<DyeingScreen> {
           onNotification: (notification) {
             if (notification is UserScrollNotification) {
               if (notification.direction == ScrollDirection.reverse) {
-                // scrolling down
                 if (_showFab) {
                   setState(() => _showFab = false);
                 }
               } else if (notification.direction == ScrollDirection.forward) {
-                // scrolling up
                 if (!_showFab) {
                   setState(() => _showFab = true);
                 }

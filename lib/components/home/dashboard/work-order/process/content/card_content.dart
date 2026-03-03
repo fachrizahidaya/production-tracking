a201e396e449bd6d08fd6039f9c28e9ee416b972
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-03-03 09:43:54 +0700

    chore: remove unnecessary

--- lib/components/home/dashboard/work-order/process/content/card_content.dart
+++ lib/components/home/dashboard/work-order/process/content/card_content.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:textile_tracking/components/home/dashboard/work-order/process/content/grid_process.dart';
 import 'package:textile_tracking/components/home/dashboard/work-order/process/content/timeline_process.dart';

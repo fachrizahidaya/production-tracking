dae95221ccb4fdf10b5a1b6324b9e1cb5a7cdbb7
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-07-16 14:23:00 +0700

    fix: rework

--- lib/components/dyeing/rework/create_form.dart
+++ lib/components/dyeing/rework/create_form.dart
@@ -6,54 +6,32 @@ import 'package:textile_tracking/components/dyeing/rework/list_form.dart';
 class CreateForm extends StatefulWidget {
   final formKey;
   final form;
-  final handleSubmit;
-  final data;
   final selectWorkOrder;
   final selectMachine;
   final id;
-  final isLoading;
 
-  const CreateForm(
-      {super.key,
-      this.formKey,
-      this.form,
-      this.handleSubmit,
-      this.data,
-      this.selectWorkOrder,
-      this.selectMachine,
-      this.id,
-      this.isLoading});
+  const CreateForm({
+    super.key,
+    this.formKey,
+    this.form,
+    this.selectWorkOrder,
+    this.selectMachine,
+    this.id,
+  });
 
   @override
   State<CreateForm> createState() => _CreateFormState();
 }
 
 class _CreateFormState extends State<CreateForm> {
-  final ValueNotifier<bool> _isSubmitting = ValueNotifier(false);
-
-  bool get _isFormIncomplete {
-    final woId = widget.form?['wo_id'];
-    final machineId = widget.form?['machine_id'];
-
-    return woId == null || machineId == null;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final attachments = (widget.form['attachments'] as List?) ?? [];
-
     return ListForm(
       formKey: widget.formKey,
       id: widget.id,
       form: widget.form,
-      data: widget.data,
-      attachments: attachments,
       selectWorkOrder: widget.selectWorkOrder,
       selectMachine: widget.selectMachine,
-      isSubmitting: _isSubmitting,
-      isFormIncomplete: _isFormIncomplete,
-      handleSubmit: widget.handleSubmit,
-      handlePickAttachments: null,
     );
   }
 }

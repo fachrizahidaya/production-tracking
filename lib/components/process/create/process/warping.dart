d91efd4f38adc174d6138dce8ad83505157d7b4e
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-07-29 09:51:02 +0700

    fix: form warping, weaving

--- lib/components/process/create/process/warping.dart
+++ lib/components/process/create/process/warping.dart
@@ -1,55 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:textile_tracking/components/master/container/template.dart';
 import 'package:textile_tracking/components/master/form/text_form.dart';
-import 'package:textile_tracking/components/master/text/no_data.dart';
 import 'package:textile_tracking/components/master/theme.dart';
 import 'package:textile_tracking/helpers/util/separated_column.dart';
 
 class WarpingSection extends StatefulWidget {
-  final items;
   final onChange;
-  final data;
+  final controller;
 
-  const WarpingSection({super.key, this.data, this.items, this.onChange});
+  const WarpingSection({super.key, this.onChange, this.controller});
 
   @override
   State<WarpingSection> createState() => _WarpingSectionState();
 }
 
 class _WarpingSectionState extends State<WarpingSection> {
-  final Map<int, TextEditingController> _beamWeightControllers = {};
-
   @override
   void initState() {
     super.initState();
   }
 
-  void _handleQty(
-    int index,
-    String value,
-  ) {
-    final safeValue = value.toString().trim().isEmpty ? '0' : value.toString();
-
-    widget.onChange(
-      index,
-      'beam_weight',
-      safeValue,
-    );
-  }
-
   @override
   void dispose() {
-    for (final controller in _beamWeightControllers.values) {
-      controller.dispose();
-    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return TemplateCard(
-        title: 'Berat Awal',
-        icon: Icons.rule,
+        title: 'Jumlah Benang',
+        icon: Icons.join_inner_outlined,
         child: _buildItemContent(context, 0));
   }
 
@@ -57,27 +37,21 @@ class _WarpingSectionState extends State<WarpingSection> {
     BuildContext context,
     int index,
   ) {
-    final isTablet = MediaQuery.of(context).size.width > 700;
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        SizedBox(
-          width: isTablet
-              ? (MediaQuery.of(context).size.width - 80) / 2
-              : double.infinity,
-          child: TextForm(
-            label: 'Jumlah Benang (KG)',
-            controller: _beamWeightControllers[index],
-            req: false,
-            isNumber: true,
-            isSorting: true,
-            handleChange: (value) => _handleQty(
-              index,
-              value,
-            ),
-          ),
+        TextForm(
+          label: 'Jumlah Benang (KG)',
+          controller: widget.controller,
+          req: false,
+          isNumber: true,
+          isSorting: true,
+          handleChange: (value) {
+            if (widget.onChange != null) {
+              widget.onChange(value);
+            }
+          },
         ),
       ].separatedBy(
         CustomTheme().vGap('lg'),
